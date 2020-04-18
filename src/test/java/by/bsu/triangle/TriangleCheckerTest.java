47d9a9b7fde84dd91266aac9e7438199c9ea11b7
George Parhomenko <per4ik35@gmail.com>
2020-04-18 15:34:30 +0300

    Create TriangleChecker and tests for it

---
+++ src/test/java/by/bsu/triangle/TriangleCheckerTest.java
@@ -0,0 +1,17 @@
+package by.bsu.triangle;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TriangleCheckerTest {
+
+    @Test
+    public void checkTrueTest(){
+        Assert.assertTrue(TriangleChecker.check(3, 4, 5));
+    }
+
+    @Test
+    public void checkFalseTest(){
+        Assert.assertFalse(TriangleChecker.check(1, 1, 2));
+    }
+}
